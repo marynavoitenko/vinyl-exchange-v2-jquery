e81a9d5d1c039a96d577811ab86ee0a276271e9b
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-03-28 03:39:11 -0400

    add current cart helper method

---
+++ app/helpers/line_items_helper.rb
@@ -0,0 +1,9 @@
+module LineItemsHelper
+  def current_cart
+    if cart = current_user.carts.find { |c| c.status != "submitted" }
+      @cart = cart
+    else
+      @cart = current_user.carts.build
+    end
+  end
+end
