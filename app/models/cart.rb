9d8280a288e62dc76dde2fb7c5bb2467f65e8024
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-03-28 04:01:54 -0400

    add model methods to calculate total and update inventory

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,9 +8,26 @@ class Cart < ApplicationRecord
     if line_item
       line_item.quantity += 1
       line_item.save
+      update_inventory(line_item.vinyl_id)
     else
       line_item = self.line_items.build(vinyl_id: item_id)
     end
     line_item
+  end
+
+  def update_inventory(vinyl_id)
+    vinyl = Vinyl.find(vinyl_id)
+    vinyl.inventory -= 1
+    vinyl.save
+  end
+
+
+  def total
+    total = 0
+    self.line_items.each do |line_item|
+      vinyl = Vinyl.find(line_item.vinyl_id)
+      total += (vinyl.price * line_item.quantity)
+    end
+    total
   end
 end
