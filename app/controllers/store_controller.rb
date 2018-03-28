ba9a7aa7a7b6ff7173d4cd670cedb6ec1bf74c78
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-03-28 02:14:58 -0400

    update to show all vinyls for sale excluding current user's

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,6 @@
 class StoreController < ApplicationController
   def index
     @genres = Genre.all
-    @vinyls = Vinyl.for_sale
+    @vinyls = Vinyl.for_sale_exclude_current_user(current_user)
   end
 end
