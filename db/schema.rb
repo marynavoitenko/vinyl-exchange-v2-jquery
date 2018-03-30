13a0334dc65593b10fe9f330be2f2552502325b5
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-03-29 23:33:41 -0400

    new user columns added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328073057) do
+ActiveRecord::Schema.define(version: 20180329072844) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -52,6 +52,9 @@ ActiveRecord::Schema.define(version: 20180328073057) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
