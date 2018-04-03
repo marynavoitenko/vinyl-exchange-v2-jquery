ece3efd643fab33636445bad11aa575af403d440
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-04-02 23:30:10 -0400

    apply error message styling to add to cart action

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -5,7 +5,7 @@ class LineItemsController < ApplicationController
         redirect_to cart_path(current_cart), {notice: 'Item added to cart!'}
       end
     else
-      redirect_to store_path, {notice: 'Item was not added. Please check inventory.'}
+      redirect_to store_path, {alert: 'Item was not added. Please check inventory.'}
     end
   end
 end
