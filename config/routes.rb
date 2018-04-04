f5e9db1161db20824f96d827f71ecf7293bb09e0
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-04-04 18:52:20 -0400

    update routes to remove unnecessary actions from line_items and carts

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'store#index', as: 'store'
 
-  resources :line_items, only: [:create, :edit, :update]
-  resources :carts, only: [:show, :edit, :update]
+  resources :line_items, only: [:create, :update]
+  resources :carts, only: [:show, :edit]
 
 end
