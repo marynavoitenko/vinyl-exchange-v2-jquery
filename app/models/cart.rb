49c1e917b846aeea33eebb532b92d9132fea3c09
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-04-04 19:59:23 -0400

    refactor

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -16,8 +16,6 @@ class Cart < ApplicationRecord
     line_item.save
   end
 
-
-
   def update_inventory(vinyl_id, qty=1)
     vinyl = Vinyl.find(vinyl_id)
     vinyl.inventory -= qty if qty <= vinyl.inventory
