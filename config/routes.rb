3d7043548e7b1f03331eda0b89bba79c1bfd8e0a
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-04-04 01:34:19 -0400

    update my_vinyls route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get '/vinyls/my_vinyls', to: 'vinyls#my_vinyls', as: 'my_vinyls'
+
   resources :vinyls
 
   resources :artists, only: [:show] do
@@ -6,11 +9,9 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'store#index', as: 'store'
-
-  get '/my_vinyls', to: 'vinyls#my_vinyls', as: 'my_vinyls'
 
   resources :line_items, only: [:create]
   resources :carts, only: [:show]
